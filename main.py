36f90fcd79d744f07d5299db1ede63281370ba75
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-11-08 14:06:29 +0900

    日本語やめて普通に書く

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+from typing import List
+from datetime import date
 
-class 装置:
-    def __init__(self, price: int, tdp: int, btc_daily_income: float):
+class Device:
+    def __init__(self, name: str, price: int, tdp: int, btc_daily_income: float):
+        self.name = name
         self.price = price
-        self.tdb = tdp
+        self.tdp = tdp
         self.btc_daily_income = btc_daily_income
 
-class 仕入台帳:
+class PurchasingLedger:
+    """仕入台帳
+    """
     def __init__(self):
-        records = []
+        self.records = []
     
-    def add(self, devices):
-        pass
+    def add(self, buy_date: date, compositions: List[Device]):
+        rec = {
+            "date": buy_date,
+            "price": sum([device.price for device in compositions]),
+            "tdp": sum([device.tdp for device in compositions]),
+            "btc_income": sum([device.btc_daily_income for device in compositions])
+        }
+        self.records.append(rec)
+
+class DeviceList:
+    def __init__(self):
+        self.devices = {}
+    
+    def add(self, device_name):
+        if not (device_name in self.devices):
+            self.devices[device_name] = 0
+        self.devices[device_name] += 1
 
 if __name__ == "__main__":
     devices = {
-        "a4000": 装置(150000, 123, 0.00007629),
-        "a5000": 装置(281790, 222, 0.00013097)
+        "a4000": Device("a4000", 150000, 123, 0.00007629),
+        "a5000": Device("a5000", 281790, 222, 0.00013097),
+        "base": Device("base", 0, 100, 0),
     }
+
+    purchasing_ledger = PurchasingLedger()
+    purchasing_ledger.add(date(2022,1,1), [devices["a5000"]] * 3 + [devices["base"]])
+    
+    print(purchasing_ledger.records)
