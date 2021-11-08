98c03a0bcb3cb938d53098be466d196c47f01d57
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-11-08 13:37:11 +0900

    試しに日本語で書いてみる

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 
-class Device:
+class 装置:
     def __init__(self, price: int, tdp: int, btc_daily_income: float):
         self.price = price
         self.tdb = tdp
         self.btc_daily_income = btc_daily_income
 
+class 仕入台帳:
+    def __init__(self):
+        records = []
+    
+    def add(self, devices):
+        pass
+
 if __name__ == "__main__":
     devices = {
-        "a4000": Device(150000, 123, 0.00007629),
-        "a5000": Device(281790, 222, 0.00013097)
+        "a4000": 装置(150000, 123, 0.00007629),
+        "a5000": 装置(281790, 222, 0.00013097)
     }
